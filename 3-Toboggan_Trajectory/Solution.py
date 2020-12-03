aa87b8c99841300eed8f10178d61cb8568395e75
Sammy Chang <sammychang2185@gmail.com>
2020-12-02 22:38:42 -0800

    day 3 problem finished

---
+++ 3-Toboggan_Trajectory/Solution.py
@@ -0,0 +1,41 @@
+# initialize problem variables
+data = []
+
+# populate data from input file
+filename = 'input.txt'
+file = open(filename, 'r')
+lines = file.readlines()
+for line in lines:
+    data.append(line.strip())
+
+# function to find how many trees are in the path given a slope
+# returns number of trees encountered
+# time complexity:
+# space complexity:
+def countTreesEncountered(x, y, terrain):
+    # +x => moves right
+    # -x => moves left
+    # +y => moves down
+    # -y => moves up
+
+    answer = 0
+    xCurr = 0
+    yCurr = 0
+
+    while yCurr < len(terrain):
+        if terrain[yCurr][xCurr] == '#':
+            answer += 1
+        xCurr = (xCurr + x) % len(terrain[0])
+        yCurr = yCurr + y
+
+    return answer
+
+print(f'Part 1 Solution: {countTreesEncountered(3, 1, data)}')
+
+a = countTreesEncountered(1, 1, data)
+b = countTreesEncountered(3, 1, data)
+c = countTreesEncountered(5, 1, data)
+d = countTreesEncountered(7, 1, data)
+e = countTreesEncountered(1, 2, data)
+
+print(f'Part 2 Solution: {a * b * c * d * e}')

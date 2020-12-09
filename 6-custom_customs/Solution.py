a19ff8ff061fddb82c2f02159075ca04928cc1a7
Sammy Chang <sammychang2185@gmail.com>
2020-12-08 16:54:10 -0800

    polished day 6 solution

--- 6-custom_customs/Solution.py
+++ 6-custom_customs/Solution.py
@@ -8,11 +8,14 @@ lines = file.readlines()
 for line in lines:
     data.append(line)
 
-
-def solution1(responses):
+# function to returns total # of unique answers people said yes to in each group
+# time complexity: 
+# space complexity: 
+def getUniqueCountPerGroup(responses):
     seen = set()
     uniqueCount = 0
     for response in responses:
+        # newline denotes a new group, so add current uniqueCount and reset seen
         if response == '\n':
             uniqueCount += len(seen)
             seen = set()
@@ -21,21 +24,32 @@ def solution1(responses):
             for char in response:
                 if char not in seen:
                     seen.add(char)
-    uniqueCount += len(seen)
+    # if there was no newline at the end of responses,
+    # add the number of elements in seen
+    if len(seen) != 0:
+        uniqueCount += len(seen)
+
     return uniqueCount
 
-
+# function that returns total # of unique answers everyone in each group said yes to
+# time complexity:
+# space complexity:
 def solution2(responses):
     seen = {}
     uniqueCount = 0
     numberOfPeopleInGroup = 0
     for response in responses:
+        # newline denotes a new group, so add current uniqueCount and reset seen
         if response == '\n':
             for key in seen.keys():
+                # if value for key in seen is equal to the # of people in a group
+                # then everyone said yes to it
                 if seen[key] == numberOfPeopleInGroup:
                     uniqueCount += 1
+            # reset for next group
             numberOfPeopleInGroup = 0
             seen = {}
+        # keep track of total people in a group and their responses
         else:
             numberOfPeopleInGroup += 1
             response = response.strip()
@@ -44,11 +58,15 @@ def solution2(responses):
                     seen[char] = 1
                 else:
                     seen[char] += 1
-    for key in seen.keys():
-        if seen[key] == numberOfPeopleInGroup:
-            uniqueCount += 1
+    # if there was no newline at the end of responses,
+    # add the number of elements in seen
+    if len(seen) != 0:
+        for key in seen.keys():
+            if seen[key] == numberOfPeopleInGroup:
+                uniqueCount += 1
+                
     return uniqueCount
 
+print(f'Part 1 Solution: {getUniqueCountPerGroup(data)}')
 
-print(f'Part 1 Solution: {solution1(data)}')
 print(f'Part 2 Solution: {solution2(data)}')

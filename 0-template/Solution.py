b7ed7337ecc96d118ebd7f8cb7dd8bc58a9a24e0
Sammy Chang <sammychang2185@gmail.com>
2021-01-03 18:18:51 -0800

    updated template folder

--- 0-template/Solution.py
+++ 0-template/Solution.py
@@ -1,38 +1,29 @@
 # initialize problem variables
 data = []
 example1 = []
-example2 = []
 
 # populate sample data from example file(s)
 filename = 'example1.txt'
 with open(filename, 'r') as file:
     lines = file.readlines()
     for line in lines:
-        example1.append(int(line.strip()))
-filename = 'example2.txt'
-with open(filename, 'r') as file:
-    lines = file.readlines()
-    for line in lines:
-        example2.append(int(line.strip()))
+        example1.append(line.strip())
 
 # populate data from input file
 filename = 'input.txt'
 with open(filename, 'r') as file:
     lines = file.readlines()
     for line in lines:
-        data.append(int(line.strip()))
+        data.append(line.strip())
 
-# function TODO
-# time complexity: TODO
-# space complexity: TODO
+
 def solution1(data):
     pass
 
-# function TODO
-# time complexity: TODO
-# space complexity: TODO
+
 def solution2(data):
     pass
+
 
 if __name__ == "__main__":
 
